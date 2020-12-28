d64f719b184730d57d6930b0465a57e5c542f60b
elchanan <elchananmahatsri@gmail.com>
2020-12-28 14:30:05 +0200

    bug fixed

--- app/src/main/java/com/example/rommies/Payment.java
+++ app/src/main/java/com/example/rommies/Payment.java
@@ -3,12 +3,16 @@ package com.example.rommies;
 import java.util.ArrayList;
 
 public class Payment {
-    private final double amount;
-    private final String Payer;
-    private final ArrayList<String> Participants;
-    private final String Reason;
-    private final Date date;
-    private final String key;
+    private double amount;
+    private String Payer;
+    private ArrayList<String> Participants;
+    private  String Reason;
+    private Date date;
+    private String key;
+
+    public Payment(){
+
+    }
 
     public Payment(String u1, double money , String r, ArrayList<String> uids, Date date,String k){//){
         this.Payer=u1;
@@ -21,14 +25,31 @@ public class Payment {
 
     public Date getDate() { return date; }
 
-    public String getKey() { return key; }
+    public String getKey() {
+        return key;
+    }
 
+    public void setDate(Date date) { this.date = date; }
 
     public ArrayList<String> getParticipant() {
         return this.Participants;
     }
 
+    public void setParticipant(ArrayList<String> uids) {
+        this.Participants = uids;
+    }
+
     public String getReason() { return this.Reason; }
+
+    public void setReason(String reason) {this.Reason = reason; }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public void setPayer(String Payer) {
+        this.Payer = Payer;
+    }
 
     public double getAmount() {
         return amount;
